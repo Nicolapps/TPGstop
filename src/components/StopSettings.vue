0960c4710793b1127fc992bf71615530fe724850
Nicolapps <webmaster@nicolapps.ch>
2016-12-31 13:59:11 +0100

    Arrêt sur demande checkbox

--- src/components/StopSettings.vue
+++ src/components/StopSettings.vue
@@ -9,6 +9,11 @@
             <label for="name">Nom de l'arrêt</label>
             <input class="form-control" type="text" v-model="stop.name" id="name">
           </div>
+          <div class="form-group">
+            <label>
+              <input type="checkbox" v-model="stop.onDemand"> Arrêt sur demande
+            </label>
+          </div>
         </div>
       </div>
     </div>
